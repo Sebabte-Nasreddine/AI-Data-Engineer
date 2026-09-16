4d0302f2974374c6b4bca4a365c06441bbb2b7c9
Sebabte-Nasreddine <sbabtinasradin@gmail.com>
2026-09-17 00:20:15 +0100

    add azure openai

--- project/ai/enrich_reviews.py
+++ project/ai/enrich_reviews.py
@@ -1,4 +1,14 @@
 import os 
 import json 
 import snowflake.connector 
-from openai import 
+from openai import OpenAI
+from azure.identity import DefaultAzureCredential, get_bearer_token_provider
+
+endpoint = "https://foragent07-resource.services.ai.azure.com/openai/v1"
+deployment_name = "gpt-5.6-sol"
+token_provider = get_bearer_token_provider(DefaultAzureCredential(), "https://ai.azure.com/.default")
+
+client = OpenAI(
+    base_url=endpoint,
+    api_key=token_provider
+)
